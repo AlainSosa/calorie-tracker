35c94b54f823af8ea863123a09682d62f98f0788
Alain Sosa <alain.sosa.v@gmail.com>
2024-10-15 09:17:38 -0400

    Proyecto finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,48 @@
-import { useReducer } from "react";
+import { useEffect, useMemo, useReducer } from "react";
 import Form from "./components/Form";
 import { activityReducer, initialState } from "./reducers/activity-reducer";
 import { ActivityList } from "./components/ActivityList";
+import CalorieTracker from "./components/CalorieTracker";
 
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState); // dispatch retorna las acciones y state retorna el estado del reducer
 
+  //este codigo se encarga de guardar el contenido en el localstorage
+  useEffect(() => {
+    localStorage.setItem("activities", JSON.stringify(state.activities));
+  }, [state.activities]);
+  //
+
+  const canRestartApp = () =>
+    useMemo(() => state.activities.length, [state.activities]);
   return (
     <>
       <header className="bg-lime-600 py-3">
-        <div className="max-w-4xl mx-auto flex justify-between ">
+        <div className="max-w-4xl mx-auto flex justify-between items-center ">
           <h1 className="text-center text-lg font-bold text-white uppercase">
             Contador de Calorias
           </h1>
+          <button
+            className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer text-sm rounded-lg disabled:opacity-10 "
+            disabled={!canRestartApp()}
+            onClick={() => dispatch({ type: "restart-app" })}
+          >
+            Reiniciar App
+          </button>
         </div>
       </header>
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
-          <Form dispatch={dispatch} />
+          <Form dispatch={dispatch} state={state} />
+        </div>
+      </section>
+      <section className="bg-gray-800 p-10 ">
+        <div className="max-w-4xl mx-auto">
+          <CalorieTracker activities={state.activities} />
         </div>
       </section>
       <section className="p-10 mx-auto max-w-4xl">
-        <ActivityList activities={state.activities} />
+        <ActivityList activities={state.activities} dispatch={dispatch} />
       </section>
     </>
   );
--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,15 +1,24 @@
 import { Activity } from "../types";
 
-export type ActivityActions = {
-  type: "save-activity";
-  payload: { newActivity: Activity }; // se puese aniadir multiples acions con la union |
+export type ActivityActions =
+  | { type: "save-activity"; payload: { newActivity: Activity } } // se puese aniadir multiples acions con la union
+  | { type: "set-activeId"; payload: { id: Activity["id"] } }
+  | { type: "delete-activeId"; payload: { id: Activity["id"] } }
+  | { type: "restart-app" };
+
+export type ActivityState = {
+  activities: Activity[];
+  activeId: Activity["id"];
 };
 
-type ActivityState = {
-  activities: Activity[];
+const localStorageActivities = (): Activity[] => {
+  const activities = localStorage.getItem("activities");
+  return activities ? JSON.parse(activities) : [];
 };
+
 export const initialState: ActivityState = {
-  activities: [],
+  activities: localStorageActivities(),
+  activeId: "",
 };
 export const activityReducer = (
   state: ActivityState = initialState,
@@ -18,9 +27,38 @@ export const activityReducer = (
   if (action.type === "save-activity") {
     // Este codigo maneja la logica para actualizar el state
     //console.log(action.payload.newActivity); // gracias a esto muestra en la consola lo que estamos ingresanto
+    let updateActivities: Activity[] = [];
+    if (state.activeId) {
+      updateActivities = state.activities.map((activity) =>
+        activity.id === state.activeId ? action.payload.newActivity : activity
+      );
+    } else {
+      updateActivities = [...state.activities, action.payload.newActivity];
+    }
     return {
       ...state,
-      activities: [...state.activities, action.payload.newActivity],
+      activities: updateActivities,
+      activeId: "",
+    };
+  }
+  if (action.type === "set-activeId") {
+    return {
+      ...state,
+      activeId: action.payload.id,
+    };
+  }
+  if (action.type === "delete-activeId") {
+    return {
+      ...state,
+      activities: state.activities.filter(
+        (activity) => activity.id !== action.payload.id
+      ),
+    };
+  }
+  if (action.type === "restart-app") {
+    return {
+      activities: [],
+      active: "",
     };
   }
   return state; // se debe poner siempre para quitar errores
